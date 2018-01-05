c41055e89fc6846c11fb4673445d5306891eb886
Mingjie.Li <Mingjie.Li@cloudcar.com>
2018-01-05 15:01:05 +0800

    pom项目名称更新

---
+++ src/main/java/io/vertx/guides/wiki/WikiConstant.java
@@ -0,0 +1,6 @@
+package io.vertx.guides.wiki;
+
+public class WikiConstant {
+  public static final String CONFIG_HTTP_SERVER_PORT = "http.server.port";
+  public static final String CONFIG_WIKIDB_QUEUE = "wikidb.queue";
+}
